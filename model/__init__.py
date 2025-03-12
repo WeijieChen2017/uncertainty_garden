2e78845223a959d6d66c423222f5426579be4af0
WinstonChen2017 <weijiechen1994@gmail.com>
2025-03-12 17:52:01 -0500

    Rename model file to unet_monai_theseus.py and update imports

--- model/__init__.py
+++ model/__init__.py
@@ -1,5 +1,5 @@
 # Import the UNet_Theseus model
-from .UNet_Theseus import UNet_Theseus
+from .unet_monai_theseus import UNet_Theseus
 
 # Make the class available at the package level
 __all__ = ["UNet_Theseus"] 
